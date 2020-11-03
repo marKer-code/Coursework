e5fe34ab5a81c049177b3208701eeb83fc449618
Maryna Kernychna <56841893+marKer-code@users.noreply.github.com>
2020-11-03 16:25:13 +0200

    Main window // Icons
    Added main winow and icons.

---
+++ Service/UI/Windows/Main.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace UI.Windows
+{
+    using System.Windows.Input;
+    using System.Windows;
+
+    public partial class Main : Window
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Avatar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
